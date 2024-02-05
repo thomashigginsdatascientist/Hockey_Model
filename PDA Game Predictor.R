eb0718683230d4828ef4d9b8bd6ae0a29c7ce101
thiggs31 <thiggins27@hotmail.com>
2024-02-05 08:25:47 -0500

    updated predictions

--- PDA Game Predictor.R
+++ PDA Game Predictor.R
@@ -249,9 +249,9 @@ train <- train[complete.cases(train),]
 # 
 # end - start
 # 
-# saveRDS(model, "C:/Users/thigg/Desktop/Hockey Models/PDA8.RDS")
+# saveRDS(model, "C:/Users/thigg/Desktop/Hockey Models/PDA9.RDS")
 
-model <- readRDS("C:/Users/thigg/Desktop/Hockey Models/PDA8.RDS")
+model <- readRDS("C:/Users/thigg/Desktop/Hockey Models/PDA9.RDS")
 
 #shapley package
 
